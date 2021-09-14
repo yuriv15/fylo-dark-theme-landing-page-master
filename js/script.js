eeff7abddd3ed6cc8713bfb9fb96998a8b3743f9
Yuri Gonçalves <65895389+yuriv15@users.noreply.github.com>
2021-09-14 10:56:35 -0300

    v1.1 - email confirmation

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 function validarEmail(){
     let email = document.querySelector('#email');
     let error = document.querySelector('#error-email');
+    let success = document.querySelector('#success-email');
     if(!email.checkValidity()){
-      error.innerHTML = "Please enter a valid email address"
+        error.innerHTML = "Please enter a valid email address"
+    } else if (email.checkValidity()) {
+        success.innerHTML = "Thank you! Sign up successful!"
     }
   }
